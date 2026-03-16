18032d5259677a40313e4e2a6645fe69578bc0ce
s36548 <s36548@pjwstk.edu.pl>
2026-03-16 23:43:00 +0100

    Add helper information about the application

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class PGO {
+    public static int getFactorial(int num) {
+        int result = 1;
+
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+System.out.println("Git hub");
+
+}
